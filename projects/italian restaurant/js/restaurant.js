e9354c505c732acb89928fcd8355f59551bf1c9f
Michelle Garcia <garcia_michelle@att.net>
2018-08-03 21:53:03 -0500

    Added animation to 3 dishes divs on scroll.

--- projects/italian restaurant/js/restaurant.js
+++ projects/italian restaurant/js/restaurant.js
@@ -1,6 +1,9 @@
 window.addEventListener('scroll', function () {
     const div1 = document.getElementById('wine');
     const div2 = document.getElementById('about');
+    const lasagnaText = document.getElementById('lasagna-text');
+    const baricole = document.getElementById('baricole');
+    const eggplant = document.getElementById('eggplant-text');
 
     if (window.scrollY > 200) {
         div1.classList.add('fade-in-up-from-left');
@@ -8,5 +11,18 @@ window.addEventListener('scroll', function () {
     } else {
         div1.classList.remove('fade-in-up-from-left');
         div2.classList.remove('fade-in-up-from-right');
+        lasagnaText.classList.remove('fade-in-up-from-left');
+    }
+
+    if (window.scrollY > 950) {
+        lasagnaText.classList.add('fade-in-up-from-left');
+        baricole.classList.add('fade-in-up');
+        eggplant.classList.add('fade-in-up-from-right');
+    } else {
+        lasagnaText.classList.remove('fade-in-up-from-left');
+        baricole.classList.remove('fade-in-up');
+        eggplant.classList.remove('fade-in-up-from-right');
+
+
     }
 });
